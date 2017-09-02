febea6b7d40cc8dbb642a95ff10dbfd6df80e648
杨然 <yangran@yangrandeMacBook-Pro.local>
2017-09-02 23:21:03 +0800

    删了

--- src/base/test/monent.js
+++
@@ -1,5 +0,0 @@
-// const moment = require('moment-jdateformatparser')
-
-// console.log(moment(Date.now()).formatWithJDF('yyyy.MM.DD hh:mm:ss'))
-
-// console.log(moment(Date.now() - 365 * 24 * 60 * 60 * 1000).formatWithJDF('yyyy.MM.DD hh:mm:ss'))
